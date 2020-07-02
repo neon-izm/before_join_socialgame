8777f2ee1f4828c0759f9c8077d9d2c23ec761b1
neon-izm <jj.y0sh1da@gmail.com>
2020-07-03 03:50:14 +0900

    sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3', 'doc4'],
-    Features: ['mdx'],
+    ソーシャルゲームのクライアントエンジニア入門以前: ['doc1', 'doc2', 'doc3', 'doc4'],
+    /*Features: ['mdx'],*/
   },
 };
