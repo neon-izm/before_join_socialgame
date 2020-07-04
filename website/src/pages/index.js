c9d08c05d69380c79efbe7382469e81c28d7bc15
neon-izm <jj.y0sh1da@gmail.com>
2020-07-04 14:57:10 +0900

    add site description (improve SEO)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="ソーシャルゲームのクライアントエンジニア入門以前">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
