c5fc91743c7a34b753e3221a06536e700044d57e
neon-izm <jj.y0sh1da@gmail.com>
2020-07-07 18:14:10 +0900

    prism

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -44,8 +44,7 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()} Before Join Socialgame Client. Built with Docusaurus.`,
     },
     prism: {
-      theme: require('prism-react-renderer/themes/github'),
-      darkTheme: require('prism-react-renderer/themes/dracula'),
+      additionalLanguages: ['charp'],
     },
     gtag: {
       trackingID: 'UA-171789087-1',
